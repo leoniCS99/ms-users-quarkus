705ea106280ed449d15bfbb8d5b2a3b3ed6ab4ee
LeoniCesar <leoni.slovak@picpaybank.com>
2024-08-08 21:10:47 -0300

    first commit

--- src/main/java/ms/controller/UserController.java
+++ src/main/java/ms/controller/UserController.java
@@ -8,6 +8,8 @@ import jakarta.ws.rs.core.Response;
 import entity.UserEntity;
 import service.UserService;
 
+import java.util.UUID;
+
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -21,7 +23,7 @@ public class UserController {
 
     @GET
     public Response findAll(@QueryParam("page") @DefaultValue("0") Integer page,
-                            @QueryParam("size") @DefaultValue("10") Integer size) {
+                            @QueryParam("size") @DefaultValue("0") Integer size) {
 
         var users = userService.findAll(page, size);
         return Response.ok(users).build();
@@ -31,5 +33,26 @@ public class UserController {
     @Transactional
     public Response createUser(UserEntity userEntity) {
         return Response.ok(userService.createUser(userEntity)).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response findById(@PathParam("id")UUID userID) {
+        return Response.ok(userService.findById(userID)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") UUID userId, UserEntity userEntity) {
+        return Response.ok(userService.updateUser(userId, userEntity)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response deleteById(@PathParam("id") UUID userId) {
+        userService.deleteById(userId);
+        return Response.noContent().build();
     }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,9 +1,11 @@
 package service;
 
 import entity.UserEntity;
+import exception.UserNotFoundException;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 public class UserService {
@@ -17,5 +19,24 @@ public class UserService {
         return UserEntity.findAll()
                 .page(page, size)
                 .list();
+    }
+
+    public UserEntity findById(UUID userId) {
+        return (UserEntity) UserEntity.findByIdOptional(userId)
+                .orElseThrow(() -> new UserNotFoundException());
+    }
+
+    public UserEntity updateUser(UUID userId, UserEntity userEntity) {
+        var user = findById(userId);
+
+        user.username = userEntity.username;
+        UserEntity.persist(user);
+
+        return user;
+    }
+
+    public void deleteById(UUID userId) {
+        var user = findById(userId);
+        UserEntity.deleteById(user.id);
     }
 }
